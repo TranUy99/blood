20ceb71834057de0997dcb443612d6f243c8ac5c
trinhb1910471 <trinhb1910471@student.ctu.edu.vn>
2023-06-08 14:42:43 +0700

    login-register

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(240, 56, 187, 76);
+const kSencondaryColor = Color(0xFF59706F);
+const kThirdColor = Color.fromARGB(232, 236, 224, 6);
+const kDarkGreyColor = Color(0xFFA8A8A8);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kZambeziColor = Color(0xFF5B5B5B);
+const kBlackColor = Color(0xFF272726);
+const kTextFieldColor = Color(0xFF979797);
+
+const kDefaultPadding = EdgeInsets.symmetric(horizontal: 30);
+
+TextStyle titleText =
+    TextStyle(color: kPrimaryColor, fontSize: 32, fontWeight: FontWeight.w700);
+TextStyle subtitle = TextStyle(
+    color: kSencondaryColor, fontSize: 18, fontWeight: FontWeight.w500);
+TextStyle textButton =
+    TextStyle(color: kThirdColor, fontSize: 18, fontWeight: FontWeight.w700);
