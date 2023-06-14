3ec2663b07b231d15f2a4ce6e52724f560e292a9
Eric <duythanh545@gmail.com>
2023-06-14 14:23:00 +0700

    Slider

--- lib/homePage/screen/product_screen.dart
+++ lib/homePage/screen/product_screen.dart
@@ -10,27 +10,56 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.5,
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 0.8,
-          crossAxisCount: 2,
-          crossAxisSpacing: 5.0,
-          mainAxisSpacing: 5.0,
-        ),
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return Container(
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 0.75,
+        crossAxisCount: 2,
+        crossAxisSpacing: 5.0,
+        mainAxisSpacing: 5.0,
+      ),
+      shrinkWrap: true,
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Detail(title: '$index'),));
+          },
+          child: Container(
             decoration: BoxDecoration(border: Border.all()),
             child: Column(
               children: [
-                Image.network('https://i.pinimg.com/736x/17/92/3e/17923e078afd1fc859746df775e346b1.jpg',)
+                Container(
+                  height: MediaQuery.of(context).size.width * 0.45,
+                  color: Colors.cyanAccent,
+                ),
+                Text('So so long title in this grid view $index'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Price'),
+                    Text('Sold number $index'),
+                  ],
+                ),
               ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
+ class Detail extends StatelessWidget {
+  final String title;
+   const Detail({Key? key, required this.title}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       body: Center(
+         child: Text(title),
+       ),
+     );
+   }
+ }
+ 
