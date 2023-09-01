60b1d4dad733e56c474cca11931653cec3ffdd47
TranUy99 <tranuy0907@gmail.com>
2023-09-01 11:09:47 +0700

    day

--- lib/src/features/home_page/view/product_screen.dart
+++ lib/src/features/home_page/view/product_screen.dart
@@ -1,17 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:mobile_store/src/constant/api_outside/api_image.dart';
 import 'package:mobile_store/src/constant/color/color.dart';
 import 'package:mobile_store/src/core/model/product.dart';
 import 'package:mobile_store/src/features/detail_product/view/detail_product_screen.dart';
-import 'package:mobile_store/src/features/detail_product/view_model/detail_product_view_model.dart';
 import 'package:mobile_store/src/features/home_page/bloc/product_bloc.dart';
 import 'package:mobile_store/src/features/home_page/view_model/product_viewmodel.dart';
-
-import '../../detail_product/bloc/detail_product_bloc.dart';
 
 class ProductScreen extends StatefulWidget {
   final ProductBloc productBloc;
